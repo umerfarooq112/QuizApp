6ab9bc382835c10f87e845baee9fce1e205ecaaf
umerKhan4ek <umerKhan4ek@gmail.com>
2021-05-11 23:47:55 +0500

    new-change

---
+++ resources/views/users.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, inventore.</p>
+</body>
+</html>
